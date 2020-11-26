4813a9d3a75ef3c3f9a89eeb3a598f2872d30c8a
Kaung Wint Shein <k028176@gmail.com>
2020-11-27 00:15:07 +0630

    footer and contact page

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 
 <head>
+  <title>Furniture store</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Furniture store</title>
   <link rel="stylesheet" href="./public/Css/bootstrap.min.css">
   <link rel="stylesheet" href="./public/Css/main.css">
 </head>
@@ -18,6 +18,11 @@
 
 
   <!-- Sithu -->
+
+
+  <!-- Kaung Wint Shein -->
+  <?php include dirname(__FILE__).'/php/includes/footer.php' ?>
+  
 
   <script src="./public/Js/jquery-3.5.1.min.js"></script>
   <script src="./public/Js/jquery-3.5.1.min.js"></script>
