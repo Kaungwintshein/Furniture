1e5b339e254d907e6f8da722f69d7339241fc28a
Kaung Htet Paing <kaunghtetpaing199@gmail.com>
2020-12-12 09:12:33 +0630

    admin

---
+++ Admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../public/css/admin.css">
+    <link rel="stylesheet" href="../public/dist/css/bootstrap.min.css">
+    
+</head>
+<body>
+   
+    
+
+
+    <script src='../public/dist/js/fontawsome.js'></script>
+    <script src='../public/dist/js/jquery-3.5.1.min.js'></script>
+    <script src='../public/dist/js/bootstrap.bundle.min.js'></script>
+</body>
+</html>
